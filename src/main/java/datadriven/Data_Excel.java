e4d7568f4715627f9cdb0b53c0e79b87900417f9
Rishabh <1rishabhjaiswal@gmail.com>
2025-04-21 21:37:24 +0530

    excel

---
+++ src/main/java/datadriven/Data_Excel.java
@@ -0,0 +1,35 @@
+package datadriven;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+public class Data_Excel {
+	public static void main(String[] args) throws IOException {
+//		declared filename and sheetname
+		String filepath= "C:\\Users\\ADMIN\\Desktop\\Codes\\MyCSV.xlsx";
+		String sheetname= "MySheet";
+//		loading of file
+		FileInputStream file=new FileInputStream(new File(filepath));
+		Workbook workbook= new XSSFWorkbook(file);
+		// load the sheet
+		Sheet sheet= workbook.getSheet(sheetname);
+		//loading the row 0
+		Row row = sheet.getRow(1);
+		
+		String username= row.getCell(0).getStringCellValue();
+		String password= row.getCell(1).getStringCellValue();
+		System.out.println(username);
+		System.out.println(password);
+		
+		
+		
+	}
+	
+
+}
